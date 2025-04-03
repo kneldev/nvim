6faa814867eb5e6e6665099550f4d09c09a76792
Kodi Nelson <k.nelsontech@gmail.com>
2025-04-03 12:35:04 -0500

    added script for relative number toggling

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Smart line number toggling when entering/exiting command mode
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+  callback = function()
+    vim.opt.relativenumber = false
+    vim.opt.number = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("CmdlineLeave", {
+  callback = function()
+    vim.opt.relativenumber = true
+  end,
+})
